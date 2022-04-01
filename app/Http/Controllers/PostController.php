c021137709d26246ddc3d9bfce093b7086f080ad
AngelZahir <engel96@comunidad.unam.mx>
2022-03-31 21:26:21 -0600

    Clase06

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -82,10 +82,13 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Posts $post)
     {
         //
-        return 'Edit: '.$id;
+        //return 'Edit: '.$id;
+        return view('dashboard.post.edit',[
+            'post' => $post
+        ]);
     }
 
     /**
---
+++ resources/views/dashboard/post/edit.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.master')
+
+@section('content')
+      @include('fragments.validation-errors')
+      @include('fragments.sesion')
+      <h1>Post</h1>
+      <form method="Post" action="{{route("post.update")}}">
+        @csrf
+      @include('dashboard.post._form')
+      </form>
+
+
+  
+
+@endsection 
